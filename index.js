84b489ea1550d6ee0935f4fec7192ee12acf1e9a
KeiferRamos <keiferramos2001@gmail.com>
2022-02-05 18:01:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const message = document.getElementById("text-generator");
+const countEl = document.getElementById("word-count");
+
+message.addEventListener("input", () => replaceText(message.value));
+
+function replaceText(message) {
+  const result = message.replace(/,/g, " ");
+  const count = result.split(" ").filter((n) => n).length;
+  countEl.textContent = `Word Count : ${count}`;
+}
