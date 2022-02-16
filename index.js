c50a81923b7f048f73fa65f7d55e6cadf24db752
KeiferRamos <98508371+KeiferRamos@users.noreply.github.com>
2022-02-16 18:52:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const countEl = document.getElementById("word-count");
 message.addEventListener("input", () => replaceText(message.value));
 
 function replaceText(message) {
-  const result = message.replace(/[,,:,.]/g, " ");
+  const result = message.replace(/[,,:,.,?,!,",;]/g, " ");
   const count = result.split(" ").filter((n) => n).length;
   countEl.textContent = `Word Count : ${count}`;
 }
